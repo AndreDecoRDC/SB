6a180fd4da3c585353818391fceb554b0c50346b
lilithgalo13 <lilithgarbazza@gmail.com>
2025-11-07 11:17:18 -0300

    Implementção da Classe Avaliação #10

--- studybridge-service/src/main/java/com/studybridge/service/Avaliação.java
+++ studybridge-service/src/main/java/com/studybridge/service/Avaliação.java
@@ -1,16 +1,39 @@
 package com.studybridge.service;
 
-public class Avaliação {
+import java.util.List;
+import com.studybridge.domain.model.*;
 
-    public void registrarAvaliações(double nota, String comentario){
+public class Avaliação{
+    private List<double> notas;
+    private List<String> comentarios;
+
+
+
+    public void registrarAvaliações(double nota, String comentario) throws NotaInvalidaException{
         if(nota < 1 || nota > 5){
-            throws new NotaInvalidaException;
+            throws new NotaInvalidaException();
         }
-
+        notas.add(nota);
+        comentarios.add(comentario);
     }
     public double calculoMediaNota(){
-        //o parametro tem que ser o array das notas de algum usuario
         //recebe o array das notas de um usuário e retorna a media do usuario
+        if (notas.isEmpty()){
+            return 0;
+        }
+        double soma = 0;
+        for (double nota : notas){
+            soma += nota;
+        }
+        return soma / notas.size();
+    }
+    public List<Double> getNotas(){
+        return notas;
+    }
+    public List<String> getComentarios(){
+        return comentarios;
+    }
+    public void integraçãoBancoDeDados(){
     }
 
 }
