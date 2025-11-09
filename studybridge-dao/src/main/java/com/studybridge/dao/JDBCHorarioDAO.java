e5c788e93a80564fe53a73711d3a6ae81d179a7d
bia-rodriguess <beatrizpiedade1@gmail.com>
2025-11-09 13:17:01 -0300

    Criação da interface HorarioDAO. #6

--- studybridge-dao/src/main/java/com/studybridge/dao/JDBCHorarioDAO.java
+++ studybridge-dao/src/main/java/com/studybridge/dao/JDBCHorarioDAO.java
@@ -1,5 +1,41 @@
 package com.studybridge.dao;
 
-public class JDBCHorarioDAO {
+import com.studybridge.domain.model.Horario;
+import java.sql.SQLException;
+import java.time.LocalTime;
+import java.util.List;
+
+public class JDBCHorarioDAO implements HorarioDAO{
+    
+    @Override
+    public Horario insertHorario(Horario horario) throws SQLException {   
+        return null;
+    }
+
+    @Override
+    public Horario getHorario(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void updateHorario(Horario horario) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteHorario(Horario horario) throws SQLException {
+        
+    }
+
+    @Override
+    public List<Horario> getAllHorarios(int monitorId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean existDuplicado(int monitorId, String diaSemana, LocalTime inicio, LocalTime fim) throws SQLException {
+        
+        return false;
+    }
     
 }
