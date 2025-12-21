3df002e1c50f8217da181ba8f3dae6faf352b625
bia-rodriguess <beatrizpiedade1@gmail.com>
2025-12-20 23:34:40 -0300

    Finalizei CSU18. Corrigi códigos anteriores, implementei busca de usuários e adicionei uma domain UsuarioAdmin. #11

--- studybridge-view-web/src/main/java/com/studybridge/web/servlet/page/AdminBuscarUsuarioServlet.java
+++ studybridge-view-web/src/main/java/com/studybridge/web/servlet/page/AdminBuscarUsuarioServlet.java
@@ -1,5 +1,7 @@
 package com.studybridge.web.servlet.page;
 
+import com.studybridge.dao.UsuarioDAO;
+import com.studybridge.domain.model.UsuarioAdmin;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/admin/busca")
 public class AdminBuscarUsuarioServlet extends HttpServlet {
@@ -17,11 +20,34 @@ public class AdminBuscarUsuarioServlet extends HttpServlet {
 
         String nome = request.getParameter("nome");
         String tipo = request.getParameter("tipo");
-        String ordenar = request.getParameter("ordenar");
+        String ordenar = request.getParameter("ordenar");        
 
-        request.setAttribute("usuarios", null);
+        boolean temFiltro
+                = (nome != null && !nome.isBlank())
+                || (tipo != null && !tipo.isBlank())
+                || (ordenar != null && !ordenar.isBlank());
+
+        if (temFiltro) {
+            UsuarioDAO dao = new UsuarioDAO();
+
+            try {
+                List<UsuarioAdmin> usuarios
+                        = dao.buscarUsuariosAdmin(nome, tipo, ordenar);
+
+                request.setAttribute("usuarios", usuarios);
+            } catch (Exception e) {
+                throw new ServletException(e);
+            }
+        } else {
+            request.setAttribute("usuarios", null);
+        }
+
+        request.setAttribute("nome", nome);
+        request.setAttribute("tipo", tipo);
+        request.setAttribute("ordenar", ordenar);
 
         request.getRequestDispatcher("/WEB-INF/views/admin/admin-busca.jsp")
-               .forward(request, response);
+                .forward(request, response);
+
     }
 }
